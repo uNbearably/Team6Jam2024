e6934f8ef86b0a7738852638dc2d6345720ec752
SpookBox5 <xela4276@gmail.com>
2024-11-23 00:11:01 -0800

    Interact system and polished movement

--- Mundane Mayhem/Assets/Code/player_code.cs
+++ Mundane Mayhem/Assets/Code/player_code.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 
 public class player_code : MonoBehaviour
 {
-    public float speed_max = 10;
+    public float speed_max = 7;
     public float speed_now = 0;
+    public float grav_now = 0;
+
+
     public Camera cam;
     public Rigidbody rb;
     public GameObject cam_target;
@@ -20,6 +23,7 @@ public class player_code : MonoBehaviour
 
     public Vector2 cursor_max;
     public Vector2 cursor_now;
+    private Vector3 raypos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -31,22 +35,26 @@ public class player_code : MonoBehaviour
     void FixedUpdate()
     {
 //move
-        if (Input.GetAxisRaw("Vertical") != 0)
-        { if (Mathf.Abs(speed_now) < speed_max)
+        if (Input.GetAxis("Vertical") != 0)
+        { 
+            if (Mathf.Abs(speed_now) < speed_max)
             { speed_now += Time.deltaTime * speed_max * Input.GetAxis("Vertical") * 5; }   //accelerate
-           
         }
+
         else if (Mathf.Abs(speed_now)>1)
-        {
-            speed_now-=Time.deltaTime*speed_max*speed_max*15*Mathf.Sign(speed_now);
-            speed_now=Mathf.Clamp(speed_now,0,100);
-        }
+        {speed_now-=Time.deltaTime*speed_max*12*Mathf.Sign(speed_now);} //decel
         else
         { speed_now = 0; }
-     //   rb.AddForce(gameObject.transform.forward * speed_now);
-        rb.linearVelocity=(gameObject.transform.forward * speed_now)+additional_force;
-        additional_force -= new Vector3(Mathf.Clamp(additional_force.x, -1, 1), Mathf.Clamp(additional_force.y, -1, 1), Mathf.Clamp(additional_force.z, -1, 1)) * Time.deltaTime;
 
+        //   rb.AddForce(gameObject.transform.forward * speed_now);
+        rb.linearVelocity = (gameObject.transform.forward * speed_now) + additional_force +grav_now*transform.up;
+        rb.angularVelocity *= 0;
+        additional_force -= new Vector3(Mathf.Clamp(additional_force.x, -1, 1), Mathf.Clamp(additional_force.y, -1, 1), Mathf.Clamp(additional_force.z, -1, 1)) * Time.deltaTime*10;
+
+//gravity
+        if (Physics.Raycast(transform.position, -transform.up, 1.2f)) {grav_now *= 0; }
+        else
+        {grav_now -= Time.deltaTime * 10;}
 
 
 
@@ -63,16 +71,32 @@ public class player_code : MonoBehaviour
 
 
 //Camera
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
         cam.transform.position = cam_target.transform.position;
         cam.transform.rotation = Quaternion.Euler(x_rot, y_rot, 0);
 //cursor
-        //RaycastHit hit;
+        cursor_now += new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"))*Time.deltaTime*5;
+        cursor_now.x = Mathf.Clamp(cursor_now.x, -cursor_max.x, cursor_max.x);
+        cursor_now.y = Mathf.Clamp(cursor_now.y, -cursor_max.y, cursor_max.y);
+        raypos = cam_target.transform.position + transform.right * cursor_now.x + transform.up * cursor_now.y;
 
-        //Debug.DrawRay(transform.position, transform.TransformDirection(-transform.up) * GroundCheckDist, Color.yellow);
-        //if (Physics.Raycast(transform.position, -transform.up, GroundCheckDist))
-        //if (Physics.Raycast(transform.position, -transform.up, out hit, GroundCheckDist))
-        //{ if (hit.transform.tag == "someTag") { hit.collider.gameObject.SetActive(false); } }
+        Debug.DrawRay(raypos, transform.TransformDirection(-cursor.transform.forward) * 10, Color.yellow);
+        RaycastHit hit;
+        if (Physics.Raycast(raypos, -cursor.transform.forward, out hit, 10)&& hit.transform.tag == "interact")
+        { 
+                //hit.collider.gameObject.SetActive(false);
+                cursor.transform.position = hit.point;
+                //cursor.transform.position = hit.transform.position;
+        }
+        else
+        { 
+            cursor.transform.position = raypos + transform.forward * 1f; 
+            cursor.transform.LookAt(cam.transform.position);
+
+        } //place cursor   
+
+
 
 
 
@@ -80,12 +104,13 @@ public class player_code : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
 //hurt
-        additional_force+=(2*(other.transform.position-gameObject.transform.position));
+        additional_force-=(4*(other.transform.position-gameObject.transform.position));
         health_now -= other.GetComponent<value_holder>().value;
         if (health_now<=0)
         {
             transform.position = new Vector3(0, 1, 0);
             speed_now *=0;
+            grav_now *= 0;
             additional_force *= 0;
             health_now = health_max;
         }
